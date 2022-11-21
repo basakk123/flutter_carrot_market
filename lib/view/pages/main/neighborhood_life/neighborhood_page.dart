e272088f502d04d93ccf63a4807539564150ef1e
basakk123 <basakk@naver.com>
2022-11-21 12:05:36 +0900

    동네생활 페이지  완료

--- lib/view/pages/main/neighborhood_life/neighborhood_page.dart
+++ lib/view/pages/main/neighborhood_life/neighborhood_page.dart
@@ -1,12 +1,39 @@
+import 'package:carrot_market/models/neighborhood_life.dart';
+import 'package:carrot_market/view/components/appbar_preferred_size.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class NeighborhoodPage extends StatelessWidget {
-  const NeighborhoodPage({Key? key}) : super(key: key);
+import 'components/life_body.dart';
+import 'components/life_header.dart';
 
+class NeighborhoodPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("NeighborhoodPage"),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('동네생활'),
+        actions: [
+          IconButton(icon: Icon(CupertinoIcons.search), onPressed: () {}),
+          IconButton(icon: Icon(CupertinoIcons.plus_rectangle_on_rectangle), onPressed: () {}),
+          IconButton(icon: Icon(CupertinoIcons.bell), onPressed: () {}),
+        ],
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: [
+          LifeHeader(),
+          ...List.generate(
+            neighborhoodLifeList.length,
+            (index) => Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: LifeBody(
+                neighborhoodLife: neighborhoodLifeList[index],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
