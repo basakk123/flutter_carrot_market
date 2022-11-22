0b578f5034b6bfbbba16c72f28e38a97af9920dc
basakk123 <basakk@naver.com>
2022-11-22 10:09:31 +0900

    나의 당근 페이지 완료

--- lib/view/pages/main/my_carrot/my_carrot_page.dart
+++ lib/view/pages/main/my_carrot/my_carrot_page.dart
@@ -1,12 +1,35 @@
+import 'package:carrot_market/models/icon_menu.dart';
+import 'package:carrot_market/view/pages/main/my_carrot/components/card_icon_menu.dart';
+import 'package:carrot_market/view/pages/main/my_carrot/components/my_carrot_header.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyCarrotPage extends StatelessWidget {
-  const MyCarrotPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("MyCarrotPage"),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text('나의 당근'),
+        actions: [
+          IconButton(icon: const Icon(Icons.settings), onPressed: () {}),
+        ],
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(0.5),
+          child: Divider(thickness: 0.5, height: 0.5, color: Colors.grey),
+        ),
+      ),
+      body: ListView(
+        children: [
+          MyCarrotHeader(),
+          const SizedBox(height: 8.0),
+          CardIconMenu(iconMenuList: iconMenu1),
+          const SizedBox(height: 8.0),
+          CardIconMenu(iconMenuList: iconMenu2),
+          const SizedBox(height: 8.0),
+          CardIconMenu(iconMenuList: iconMenu3)
+        ],
+      ),
     );
   }
 }
