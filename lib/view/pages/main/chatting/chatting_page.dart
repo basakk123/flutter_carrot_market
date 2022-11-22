d6fd040afc56a6eddf699af2dcd415f70a996fff
basakk123 <basakk@naver.com>
2022-11-22 10:02:19 +0900

    채팅 페이지 완료

--- lib/view/pages/main/chatting/chatting_page.dart
+++ lib/view/pages/main/chatting/chatting_page.dart
@@ -1,12 +1,22 @@
+import 'package:carrot_market/models/chat_message.dart';
+import 'package:carrot_market/view/components/appbar_preferred_size.dart';
+import 'package:carrot_market/view/pages/main/chatting/components/chat_container.dart';
 import 'package:flutter/material.dart';
 
 class ChattingPage extends StatelessWidget {
-  const ChattingPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("chattingPage"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('채팅'),
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: List.generate(
+          chatMessageList.length,
+          (index) => ChatContainer(chatMessage: chatMessageList[index]),
+        ),
+      ),
     );
   }
 }
